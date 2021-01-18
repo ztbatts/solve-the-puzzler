6189d52db90ccbbbb52c6b5de02287391fce8989
Zach Batts <ztbatts@gmail.com>
2021-01-17 21:15:43 -0500

    Check if they're are multiple solutions

--- python/puzzlers/2021-01-17_landmarks_elements_states/Solution.py
+++ python/puzzlers/2021-01-17_landmarks_elements_states/Solution.py
@@ -11,12 +11,14 @@ def extractFirstWordFromList(pathToFile):
         elements.append(firstWord)
     return elements
 
-def findSolution():
+
+def findSolutions():
     landmarks = ['hooverdam', 'koreanwar']  # any other ideas?
     atomicElements = extractFirstWordFromList('/home/zach/solve-the-puzzler/datasets/atomic_elements.txt')
     states = extractFirstWordFromList('/home/zach/solve-the-puzzler/datasets/US_States.txt')
 
-    # Try a brute force approach
+    # Brute force approach
+    solutions = []
     for landmark in landmarks:
         for elem in atomicElements:
             chars = landmark + elem
@@ -25,12 +27,14 @@ def findSolution():
                 print('Landmark: ' + landmark + ', Element: ' + elem + ', states: ' + combo[0] + ', ' + combo[1])
                 if sorted(combo[0] + combo[1]) == sorted(chars):
                     print('Found the two states!')
-                    return elem, combo[0], combo[1]
+                    solutions.append((landmark, elem, combo[0], combo[1]))
 
-
-
-    print("Job's done!")
+    return solutions
 
 
 if __name__ == "__main__":
-    elem, firstState, secondState = findSolution()
+    sols = findSolutions()
+    print("Job's done!")
+    for sol in sols:
+        landmark, elem, firstState, secondState = sol
+        print('Landmark: ' + landmark + ', Element: ' + elem + ', states: ' + firstState + ', ' + secondState)
